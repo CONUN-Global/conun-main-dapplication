2e148a8ecbc44a73bb11f0da8f695168e2df31b6
Ivan Saldano <ivanms1@gmail.com>
2021-05-04 18:52:35 +0900

    remove date from logger

--- electron/logger/index.ts
+++ electron/logger/index.ts
@@ -20,7 +20,6 @@ async function logger(name: string, error: any) {
   const body = {
     productName: "Conun Manager",
     companyName: "CONUN Global",
-    error_date: Date.now().toLocaleString(),
     version: app.getVersion(),
     platform: process.platform,
     process_type: process.type,
